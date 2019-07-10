3bbc9cddd671438da73c4b1a7bc4ee4f7560866a
Alan Dragomirecký <alan@dragomirecky.com>
2019-07-11 01:13:48 +0200

    improve formatting

--- Package.swift
+++ Package.swift
@@ -1,18 +1,18 @@
 // swift-tools-version:4.2
-
 import PackageDescription
 
 let package = Package(
     name: "Semihosting",
     products: [
         .library(
-        name: "Semihosting",
-        type: .static,
-        targets: ["Semihosting"]),
+            name: "Semihosting",
+            targets: ["Semihosting"]
+        ),
     ],
     targets: [
         .target(
             name: "Semihosting",
-            dependencies: []),
+            dependencies: []
+        ),
     ]
 )
